indexmap-rs/indexmap

Add range support to `extract_if`

src/map.rs
@@ -307,7 +307,8 @@ impl<K, V, S> IndexMap<K, V, S> {
         Drain::new(self.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    /// Creates an iterator which uses a closure to determine if an element should be removed,
+    /// for all elements in the given range.
     ///
     /// If the closure returns true, the element is removed from the map and yielded.
     /// If the closure returns false, or panics, the element remains in the map and will not be
@@ -315,6 +316,11 @@ impl<K, V, S> IndexMap<K, V, S> {
     ///
     /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
     /// whether you choose to keep or remove it.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire map, use `RangeFull`
+    /// like `map.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -330,7 +336,7 @@ impl<K, V, S> IndexMap<K, V, S> {
     /// use indexmap::IndexMap;
     ///
     /// let mut map: IndexMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
-    /// let extracted: IndexMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    /// let extracted: IndexMap<i32, i32> = map.extract_if(.., |k, _v| k % 2 == 0).collect();
     ///
     /// let evens = extracted.keys().copied().collect::<Vec<_>>();
     /// let odds = map.keys().copied().collect::<Vec<_>>();
@@ -338,11 +344,12 @@ impl<K, V, S> IndexMap<K, V, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    pub fn extract_if<R, F>(&mut self, range: R, pred: F) -> ExtractIf<'_, K, V, F>
     where
+        R: RangeBounds<usize>,
         F: FnMut(&K, &mut V) -> bool,
     {
-        ExtractIf::new(&mut self.core, pred)
+        ExtractIf::new(&mut self.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/set.rs
@@ -257,11 +257,17 @@ impl<T, S> IndexSet<T, S> {
         Drain::new(self.map.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if a value should be removed.
+    /// Creates an iterator which uses a closure to determine if a value should be removed,
+    /// for all values in the given range.
     ///
     /// If the closure returns true, then the value is removed and yielded.
     /// If the closure returns false, the value will remain in the list and will not be yielded
     /// by the iterator.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire set, use `RangeFull`
+    /// like `set.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -277,7 +283,7 @@ impl<T, S> IndexSet<T, S> {
     /// use indexmap::IndexSet;
     ///
     /// let mut set: IndexSet<i32> = (0..8).collect();
-    /// let extracted: IndexSet<i32> = set.extract_if(|v| v % 2 == 0).collect();
+    /// let extracted: IndexSet<i32> = set.extract_if(.., |v| v % 2 == 0).collect();
     ///
     /// let evens = extracted.into_iter().collect::<Vec<_>>();
     /// let odds = set.into_iter().collect::<Vec<_>>();
@@ -285,11 +291,12 @@ impl<T, S> IndexSet<T, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, T, F>
+    pub fn extract_if<R, F>(&mut self, range: R, pred: F) -> ExtractIf<'_, T, F>
     where
+        R: RangeBounds<usize>,
         F: FnMut(&T) -> bool,
     {
-        ExtractIf::new(&mut self.map.core, pred)
+        ExtractIf::new(&mut self.map.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

indexmap-rs/ordermap

(cherry picked from commit a8d7dc50a250971fe2203bdf23c2b5a307db7c86)

src/map.rs
@@ -282,7 +282,8 @@ impl<K, V, S> OrderMap<K, V, S> {
         self.inner.drain(range)
     }
 
-    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    /// Creates an iterator which uses a closure to determine if an element should be removed,
+    /// for all elements in the given range.
     ///
     /// If the closure returns true, the element is removed from the map and yielded.
     /// If the closure returns false, or panics, the element remains in the map and will not be
@@ -290,6 +291,11 @@ impl<K, V, S> OrderMap<K, V, S> {
     ///
     /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
     /// whether you choose to keep or remove it.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire map, use `RangeFull`
+    /// like `map.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -305,7 +311,7 @@ impl<K, V, S> OrderMap<K, V, S> {
     /// use ordermap::OrderMap;
     ///
     /// let mut map: OrderMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
-    /// let extracted: OrderMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    /// let extracted: OrderMap<i32, i32> = map.extract_if(.., |k, _v| k % 2 == 0).collect();
     ///
     /// let evens = extracted.keys().copied().collect::<Vec<_>>();
     /// let odds = map.keys().copied().collect::<Vec<_>>();
@@ -313,11 +319,12 @@ impl<K, V, S> OrderMap<K, V, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    pub fn extract_if<F, R>(&mut self, range: R, pred: F) -> ExtractIf<'_, K, V, F>
     where
         F: FnMut(&K, &mut V) -> bool,
+        R: RangeBounds<usize>,
     {
-        self.inner.extract_if(.., pred)
+        self.inner.extract_if(range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/set.rs
@@ -234,11 +234,17 @@ impl<T, S> OrderSet<T, S> {
         self.inner.drain(range)
     }
 
-    /// Creates an iterator which uses a closure to determine if a value should be removed.
+    /// Creates an iterator which uses a closure to determine if a value should be removed,
+    /// for all values in the given range.
     ///
     /// If the closure returns true, then the value is removed and yielded.
     /// If the closure returns false, the value will remain in the list and will not be yielded
     /// by the iterator.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire set, use `RangeFull`
+    /// like `set.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -254,7 +260,7 @@ impl<T, S> OrderSet<T, S> {
     /// use ordermap::OrderSet;
     ///
     /// let mut set: OrderSet<i32> = (0..8).collect();
-    /// let extracted: OrderSet<i32> = set.extract_if(|v| v % 2 == 0).collect();
+    /// let extracted: OrderSet<i32> = set.extract_if(.., |v| v % 2 == 0).collect();
     ///
     /// let evens = extracted.into_iter().collect::<Vec<_>>();
     /// let odds = set.into_iter().collect::<Vec<_>>();
@@ -262,11 +268,12 @@ impl<T, S> OrderSet<T, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, T, F>
+    pub fn extract_if<F, R>(&mut self, range: R, pred: F) -> ExtractIf<'_, T, F>
     where
         F: FnMut(&T) -> bool,
+        R: RangeBounds<usize>,
     {
-        self.inner.extract_if(.., pred)
+        self.inner.extract_if(range, pred)
     }
 
     /// Splits the collection into two at the given index.