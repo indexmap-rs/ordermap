indexmap-rs/indexmap

Add `is_sorted`{,`_by`,`_by_key`}

src/map/tests.rs
@@ -1264,3 +1264,49 @@ fn insert_sorted_by() {
     }
     assert_eq!(values, *map.as_slice());
 }
+
+#[test]
+fn is_sorted() {
+    fn expect(map: &IndexMap<i32, i32>, e: [bool; 7]) {
+        assert_eq!(e[0], map.is_sorted());
+        assert_eq!(e[1], map.is_sorted_by(|k1, _, k2, _| k1 < k2));
+        assert_eq!(e[2], map.is_sorted_by(|k1, _, k2, _| k1 > k2));
+        assert_eq!(e[3], map.is_sorted_by(|_, v1, _, v2| v1 < v2));
+        assert_eq!(e[4], map.is_sorted_by(|_, v1, _, v2| v1 > v2));
+        assert_eq!(e[5], map.is_sorted_by_key(|k, _| k));
+        assert_eq!(e[6], map.is_sorted_by_key(|_, v| v));
+    }
+
+    let mut map = IndexMap::from_iter((0..10).map(|i| (i, i * i)));
+    expect(&map, [true, true, false, true, false, true, true]);
+
+    map[5] = -1;
+    expect(&map, [true, true, false, false, false, true, false]);
+
+    map[5] = 25;
+    map.replace_index(5, -1).unwrap();
+    expect(&map, [false, false, false, true, false, false, true]);
+}
+
+#[test]
+fn is_sorted_trivial() {
+    fn expect(map: &IndexMap<i32, i32>, e: [bool; 5]) {
+        assert_eq!(e[0], map.is_sorted());
+        assert_eq!(e[1], map.is_sorted_by(|_, _, _, _| true));
+        assert_eq!(e[2], map.is_sorted_by(|_, _, _, _| false));
+        assert_eq!(e[3], map.is_sorted_by_key(|_, _| 0f64));
+        assert_eq!(e[4], map.is_sorted_by_key(|_, _| f64::NAN));
+    }
+
+    let mut map = IndexMap::new();
+    expect(&map, [true, true, true, true, true]);
+
+    map.insert(0, 0);
+    expect(&map, [true, true, true, true, true]);
+
+    map.insert(1, 1);
+    expect(&map, [true, true, false, true, false]);
+
+    map.reverse();
+    expect(&map, [false, true, false, true, false]);
+}

src/set/tests.rs
@@ -1019,3 +1019,42 @@ fn test_partition_point() {
     assert_eq!(b.partition_point(|&x| x < 7), 2);
     assert_eq!(b.partition_point(|&x| x < 8), 3);
 }
+
+#[test]
+fn is_sorted() {
+    fn expect(set: &IndexSet<i32>, e: [bool; 4]) {
+        assert_eq!(e[0], set.is_sorted());
+        assert_eq!(e[1], set.is_sorted_by(|v1, v2| v1 < v2));
+        assert_eq!(e[2], set.is_sorted_by(|v1, v2| v1 > v2));
+        assert_eq!(e[3], set.is_sorted_by_key(|v| v));
+    }
+
+    let mut set = IndexSet::<i32>::from_iter(0..10);
+    expect(&set, [true, true, false, true]);
+
+    set.replace_index(5, -1).unwrap();
+    expect(&set, [false, false, false, false]);
+}
+
+#[test]
+fn is_sorted_trivial() {
+    fn expect(set: &IndexSet<i32>, e: [bool; 5]) {
+        assert_eq!(e[0], set.is_sorted());
+        assert_eq!(e[1], set.is_sorted_by(|_, _| true));
+        assert_eq!(e[2], set.is_sorted_by(|_, _| false));
+        assert_eq!(e[3], set.is_sorted_by_key(|_| 0f64));
+        assert_eq!(e[4], set.is_sorted_by_key(|_| f64::NAN));
+    }
+
+    let mut set = IndexSet::<i32>::default();
+    expect(&set, [true, true, true, true, true]);
+
+    set.insert(0);
+    expect(&set, [true, true, true, true, true]);
+
+    set.insert(1);
+    expect(&set, [true, true, false, true, false]);
+
+    set.reverse();
+    expect(&set, [false, true, false, true, false]);
+}

indexmap-rs/ordermap

(cherry picked from commit e4bb7d03b92ad7768fb26b5f062fefda6af0dc72)

src/map/tests.rs
@@ -1246,3 +1246,49 @@ fn insert_sorted_by() {
     }
     assert_eq!(values, *map.as_slice());
 }
+
+#[test]
+fn is_sorted() {
+    fn expect(map: &OrderMap<i32, i32>, e: [bool; 7]) {
+        assert_eq!(e[0], map.is_sorted());
+        assert_eq!(e[1], map.is_sorted_by(|k1, _, k2, _| k1 < k2));
+        assert_eq!(e[2], map.is_sorted_by(|k1, _, k2, _| k1 > k2));
+        assert_eq!(e[3], map.is_sorted_by(|_, v1, _, v2| v1 < v2));
+        assert_eq!(e[4], map.is_sorted_by(|_, v1, _, v2| v1 > v2));
+        assert_eq!(e[5], map.is_sorted_by_key(|k, _| k));
+        assert_eq!(e[6], map.is_sorted_by_key(|_, v| v));
+    }
+
+    let mut map = OrderMap::from_iter((0..10).map(|i| (i, i * i)));
+    expect(&map, [true, true, false, true, false, true, true]);
+
+    map[5] = -1;
+    expect(&map, [true, true, false, false, false, true, false]);
+
+    map[5] = 25;
+    map.replace_index(5, -1).unwrap();
+    expect(&map, [false, false, false, true, false, false, true]);
+}
+
+#[test]
+fn is_sorted_trivial() {
+    fn expect(map: &OrderMap<i32, i32>, e: [bool; 5]) {
+        assert_eq!(e[0], map.is_sorted());
+        assert_eq!(e[1], map.is_sorted_by(|_, _, _, _| true));
+        assert_eq!(e[2], map.is_sorted_by(|_, _, _, _| false));
+        assert_eq!(e[3], map.is_sorted_by_key(|_, _| 0f64));
+        assert_eq!(e[4], map.is_sorted_by_key(|_, _| f64::NAN));
+    }
+
+    let mut map = OrderMap::new();
+    expect(&map, [true, true, true, true, true]);
+
+    map.insert(0, 0);
+    expect(&map, [true, true, true, true, true]);
+
+    map.insert(1, 1);
+    expect(&map, [true, true, false, true, false]);
+
+    map.reverse();
+    expect(&map, [false, true, false, true, false]);
+}

src/set/tests.rs
@@ -1026,3 +1026,42 @@ fn test_partition_point() {
     assert_eq!(b.partition_point(|&x| x < 7), 2);
     assert_eq!(b.partition_point(|&x| x < 8), 3);
 }
+
+#[test]
+fn is_sorted() {
+    fn expect(set: &OrderSet<i32>, e: [bool; 4]) {
+        assert_eq!(e[0], set.is_sorted());
+        assert_eq!(e[1], set.is_sorted_by(|v1, v2| v1 < v2));
+        assert_eq!(e[2], set.is_sorted_by(|v1, v2| v1 > v2));
+        assert_eq!(e[3], set.is_sorted_by_key(|v| v));
+    }
+
+    let mut set = OrderSet::<i32>::from_iter(0..10);
+    expect(&set, [true, true, false, true]);
+
+    set.replace_index(5, -1).unwrap();
+    expect(&set, [false, false, false, false]);
+}
+
+#[test]
+fn is_sorted_trivial() {
+    fn expect(set: &OrderSet<i32>, e: [bool; 5]) {
+        assert_eq!(e[0], set.is_sorted());
+        assert_eq!(e[1], set.is_sorted_by(|_, _| true));
+        assert_eq!(e[2], set.is_sorted_by(|_, _| false));
+        assert_eq!(e[3], set.is_sorted_by_key(|_| 0f64));
+        assert_eq!(e[4], set.is_sorted_by_key(|_| f64::NAN));
+    }
+
+    let mut set = OrderSet::<i32>::default();
+    expect(&set, [true, true, true, true, true]);
+
+    set.insert(0);
+    expect(&set, [true, true, true, true, true]);
+
+    set.insert(1);
+    expect(&set, [true, true, false, true, false]);
+
+    set.reverse();
+    expect(&set, [false, true, false, true, false]);
+}