indexmap-rs/indexmap

Make use of RFC2145 type privacy for sealed traits

src/map/mutable.rs
@@ -18,7 +18,8 @@ use super::{
 /// `use` this trait to enable its methods for `IndexMap`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableKeys: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableKeys: Sealed {
     type Key;
     type Value;
 
@@ -103,7 +104,8 @@ where
 /// `use` this trait to enable its methods for `Entry`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableEntryKey: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableEntryKey: Sealed {
     type Key;
 
     /// Gets a mutable reference to the entry's key, either within the map if occupied,
@@ -154,12 +156,10 @@ impl<K, V> MutableEntryKey for IndexedEntry<'_, K, V> {
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<K, V, S> Sealed for super::IndexMap<K, V, S> {}
-    impl<K, V> Sealed for super::Entry<'_, K, V> {}
-    impl<K, V> Sealed for super::OccupiedEntry<'_, K, V> {}
-    impl<K, V> Sealed for super::VacantEntry<'_, K, V> {}
-    impl<K, V> Sealed for super::IndexedEntry<'_, K, V> {}
-}
+impl<K, V, S> Sealed for IndexMap<K, V, S> {}
+impl<K, V> Sealed for Entry<'_, K, V> {}
+impl<K, V> Sealed for OccupiedEntry<'_, K, V> {}
+impl<K, V> Sealed for VacantEntry<'_, K, V> {}
+impl<K, V> Sealed for IndexedEntry<'_, K, V> {}

src/map/core/raw_entry_v1.rs
@@ -20,7 +20,8 @@ use hashbrown::hash_table;
 /// Opt-in access to the experimental raw entry API.
 ///
 /// See the [`raw_entry_v1`][self] module documentation for more information.
-pub trait RawEntryApiV1<K, V, S>: private::Sealed {
+#[expect(private_bounds)]
+pub trait RawEntryApiV1<K, V, S>: Sealed {
     /// Creates a raw immutable entry builder for the [`IndexMap`].
     ///
     /// Raw entries provide the lowest level of control for searching and
@@ -646,8 +647,6 @@ impl<'a, K, V, S> RawVacantEntryMut<'a, K, V, S> {
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<K, V, S> Sealed for super::IndexMap<K, V, S> {}
-}
+impl<K, V, S> Sealed for IndexMap<K, V, S> {}

src/set/mutable.rs
@@ -17,7 +17,8 @@ use crate::map::MutableKeys;
 /// `use` this trait to enable its methods for `IndexSet`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableValues: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableValues: Sealed {
     type Value;
 
     /// Return item index and mutable reference to the value
@@ -79,8 +80,6 @@ where
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<T, S> Sealed for super::IndexSet<T, S> {}
-}
+impl<T, S> Sealed for IndexSet<T, S> {}

indexmap-rs/ordermap

(cherry picked from commit 4849b1679f6594112805ef8d542e230dfb4c37cf)

src/map/mutable.rs
@@ -17,7 +17,8 @@ use indexmap::map::MutableKeys as _;
 /// `use` this trait to enable its methods for `OrderMap`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableKeys: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableKeys: Sealed {
     type Key;
     type Value;
 
@@ -97,7 +98,8 @@ where
 /// `use` this trait to enable its methods for `Entry`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableEntryKey: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableEntryKey: Sealed {
     type Key;
 
     /// Gets a mutable reference to the entry's key, either within the map if occupied,
@@ -148,12 +150,10 @@ impl<K, V> MutableEntryKey for IndexedEntry<'_, K, V> {
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<K, V, S> Sealed for super::OrderMap<K, V, S> {}
-    impl<K, V> Sealed for super::Entry<'_, K, V> {}
-    impl<K, V> Sealed for super::OccupiedEntry<'_, K, V> {}
-    impl<K, V> Sealed for super::VacantEntry<'_, K, V> {}
-    impl<K, V> Sealed for super::IndexedEntry<'_, K, V> {}
-}
+impl<K, V, S> Sealed for OrderMap<K, V, S> {}
+impl<K, V> Sealed for Entry<'_, K, V> {}
+impl<K, V> Sealed for OccupiedEntry<'_, K, V> {}
+impl<K, V> Sealed for VacantEntry<'_, K, V> {}
+impl<K, V> Sealed for IndexedEntry<'_, K, V> {}

src/map/raw_entry_v1.rs
@@ -21,7 +21,8 @@ use alloc::vec::Vec;
 /// Opt-in access to the experimental raw entry API.
 ///
 /// See the [`raw_entry_v1`][self] module documentation for more information.
-pub trait RawEntryApiV1<K, V, S>: private::Sealed {
+#[expect(private_bounds)]
+pub trait RawEntryApiV1<K, V, S>: Sealed {
     /// Creates a raw immutable entry builder for the [`OrderMap`].
     ///
     /// Raw entries provide the lowest level of control for searching and
@@ -593,8 +594,6 @@ impl<'a, K, V, S> RawVacantEntryMut<'a, K, V, S> {
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<K, V, S> Sealed for super::OrderMap<K, V, S> {}
-}
+impl<K, V, S> Sealed for OrderMap<K, V, S> {}

src/set/mutable.rs
@@ -16,7 +16,8 @@ use indexmap::set::MutableValues as _;
 /// `use` this trait to enable its methods for `OrderSet`.
 ///
 /// This trait is sealed and cannot be implemented for types outside this crate.
-pub trait MutableValues: private::Sealed {
+#[expect(private_bounds)]
+pub trait MutableValues: Sealed {
     type Value;
 
     /// Return item index and mutable reference to the value
@@ -72,8 +73,6 @@ where
     }
 }
 
-mod private {
-    pub trait Sealed {}
+trait Sealed {}
 
-    impl<T, S> Sealed for super::OrderSet<T, S> {}
-}
+impl<T, S> Sealed for OrderSet<T, S> {}