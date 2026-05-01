46abf470ad88dbcecde79f0ac18ffa7000f11014
Josh Stone <cuviper@gmail.com>
2026-05-01 16:48:15 -0700

    Improve the example of `get_disjoint_mut`
    (cherry picked from commit ddb0316dc25e12fb0ecda55f0a4e754dd62e6e5c)

--- src/map.rs
+++ src/map.rs
@@ -820,7 +820,10 @@ where
     ///
     /// ```
     /// let mut map = ordermap::OrderMap::from([(1, 'a'), (3, 'b'), (2, 'c')]);
-    /// assert_eq!(map.get_disjoint_mut([&2, &1]), [Some(&mut 'c'), Some(&mut 'a')]);
+    /// assert_eq!(
+    ///   map.get_disjoint_mut([&2, &1, &0]),
+    ///   [Some(&mut 'c'), Some(&mut 'a'), None],
+    /// );
     /// ```
     #[track_caller]
     pub fn get_disjoint_mut<Q, const N: usize>(&mut self, keys: [&Q; N]) -> [Option<&mut V>; N]
