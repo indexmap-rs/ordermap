323b6797f047494cee90ff5e47faf1e742192671
maxtaran2010 <ocotifuzo727@gmail.com>
2026-07-10 00:44:16 +0300

    Fix typos in comments and documentation

--- src/map/slice.rs
+++ src/map/slice.rs
@@ -244,7 +244,7 @@ impl<K, V> Slice<K, V> {
         Values::new(&self.entries)
     }
 
-    /// Return an iterator over mutable references to the the values of the map slice.
+    /// Return an iterator over mutable references to the values of the map slice.
     pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
         ValuesMut::new(&mut self.entries)
     }
