c95da18c97d2742fe8731f36e81c1d04f0620c6a
Josh Stone <cuviper@gmail.com>
2026-08-28 11:02:43 -0700

    fix clippy::derivable_impls

--- src/map/tests.rs
+++ src/map/tests.rs
@@ -374,16 +374,11 @@ fn entry_and_modify() {
 fn entry_or_default() {
     let mut map = IndexMap::new();
 
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, Default, PartialEq)]
     enum TestEnum {
+        #[default]
         DefaultValue,
         NonDefaultValue,
-    }
-
-    impl Default for TestEnum {
-        fn default() -> Self {
-            TestEnum::DefaultValue
-        }
     }
 
     map.insert(1, TestEnum::NonDefaultValue);
