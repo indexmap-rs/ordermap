21963654e41f86f396a2ba6d9582ccd8eb7df52c
Josh Stone <cuviper@gmail.com>
2026-08-28 11:01:26 -0700

    fix clippy::useless_vec (and more) in tests

--- src/set/tests.rs
+++ src/set/tests.rs
@@ -42,8 +42,8 @@ fn insert() {
 
 #[test]
 fn insert_full() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = IndexSet::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -90,8 +90,7 @@ fn insert_2() {
 
 #[test]
 fn insert_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: IndexSet<u8> = elements.drain(..).collect();
+    let mut set = IndexSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -175,8 +174,8 @@ fn replace() {
 
 #[test]
 fn replace_full() {
-    let replace = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let replace = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = IndexSet::with_capacity(replace.len());
 
     for (i, &elt) in replace.iter().enumerate() {
@@ -223,8 +222,7 @@ fn replace_2() {
 
 #[test]
 fn replace_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: IndexSet<u8> = elements.drain(..).collect();
+    let mut set = IndexSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -426,9 +424,9 @@ fn partial_eq_and_eq() {
 #[test]
 fn extend() {
     let mut set = IndexSet::new();
-    set.extend(vec![&1, &2, &3, &4]);
-    set.extend(vec![5, 6]);
-    assert_eq!(set.into_iter().collect::<Vec<_>>(), vec![1, 2, 3, 4, 5, 6]);
+    set.extend([&1, &2, &3, &4]);
+    set.extend([5, 6]);
+    assert_eq!(set.into_iter().collect::<Vec<_>>(), [1, 2, 3, 4, 5, 6]);
 }
 
 #[test]
